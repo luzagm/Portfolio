10f779d8130c18717294c49bd35c918feb1a16e1
luzagm <luz.aguirre.mo@gmail.com>
2019-10-15 17:19:40 +0200

    add contact modal and fixed design

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,38 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const input = document.querySelector(".js-input");
+const label = document.querySelector(".label__txt");
+const modal = document.getElementById("myModal");
+const openModalBtn = document.getElementById("modal-trigger");
+const closeModalBtn = document.querySelector(".js-close-button");
+
+//Input
+// function labelChange(ev) {
+//   const inputValue = ev.currentTarget.value;
+
+//   if (inputValue === "") {
+//     label.classList.contains("label__txt");
+//   } else {
+//     label.classList.add("validInput");
+//   }
+// }
+
+// Modal
+function openModal(ev) {
+  ev.preventDefault();
+  if (input.value !== "") {
+    modal.classList.remove("is-hidden");
+  }
+}
+
+function closeModal() {
+  modal.classList.add("is-hidden");
+}
+
+function closeFromWindow() {
+  modal.classList.add("is-hidden");
+}
+
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
+modal.addEventListener("click", closeFromWindow);
